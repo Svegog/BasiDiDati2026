56a5538ff027b91afc160977729ae9fedba4ee38
Diego Montalti <diego.montalti@studio.unibo.it>
2026-06-21 02:10:11 +0200

    Correzioni di stile, estetica e aggiunta logo (definitivo?)

--- appStabilimento/gestionaleStabilimento/settings.py
+++ appStabilimento/gestionaleStabilimento/settings.py
@@ -107,6 +107,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [APP_DIR / 'static']
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
